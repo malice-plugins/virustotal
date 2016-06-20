b076104fd0a6f81fa03a25af919b07b21496545a
blacktop <blacktop@users.noreply.github.com>
2016-06-19 23:42:44 -0600

    workin on adding rethinkdb

--- virustotal.go
+++ virustotal.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"os"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 	"github.com/crackcomm/go-clitable"
 	"github.com/levigross/grequests"
+	"github.com/mitchellh/mapstructure"
 	"github.com/parnurzeal/gorequest"
+	r "gopkg.in/dancannon/gorethink.v2"
 )
 
 // Version stores the plugin's version
@@ -18,6 +22,17 @@ var Version string
 
 // BuildTime stores the plugin's build time
 var BuildTime string
+
+// VirusTotal is a type
+type VirusTotal struct {
+	Data interface{} `json:"data" gorethink:"data"`
+	Now  string      `json:"now" gorethink:"now"`
+}
+
+type pluginResults struct {
+	ID string     `json:"id" gorethink:"id,omitempty"`
+	VT VirusTotal `json:"virustotal" gorethink:"virustotal"`
+}
 
 // virustotal json object
 type virustotal struct {
@@ -88,22 +103,40 @@ func assert(err error) {
 	}
 }
 
+// getSHA256 calculates a file's sha256sum
+func getSHA256(name string) string {
+
+	dat, err := ioutil.ReadFile(name)
+	assert(err)
+
+	h256 := sha256.New()
+	_, err = h256.Write(dat)
+	assert(err)
+
+	return fmt.Sprintf("%x", h256.Sum(nil))
+}
+
 func printStatus(resp gorequest.Response, body string, errs []error) {
 	fmt.Println(resp.Status)
 }
 
-func printMarkDownTable(virustotal virustotal) {
+func printMarkDownTable(virustotal VirusTotal) {
+
+	var vt ResultsData
+	err := mapstructure.Decode(virustotal.Data, &vt)
+	assert(err)
+
 	fmt.Println("#### VirusTotal")
-	if virustotal.Results.ResponseCode == 0 {
+	if vt.ResponseCode == 0 {
 		fmt.Println(" - Not found")
 	} else {
 		table := clitable.New([]string{"Ratio", "Link", "API", "Scanned"})
 		table.AddRow(map[string]interface{}{
-			"Ratio": getRatio(virustotal.Results.Positives, virustotal.Results.Total),
-			"Link":  fmt.Sprintf("[link](%s)", virustotal.Results.Permalink),
+			"Ratio": getRatio(vt.Positives, vt.Total),
+			"Link":  fmt.Sprintf("[link](%s)", vt.Permalink),
 			"API":   "Public",
-			// "API":     virustotal.ApiType,
-			"Scanned": virustotal.Results.ScanDate,
+			// "API":     vt.ApiType,
+			"Scanned": vt.ScanDate,
 		})
 		table.Markdown = true
 		table.Print()
@@ -168,7 +201,7 @@ func scanFile(path string, apikey string) string {
 }
 
 // lookupHash retreieves the virustotal file report for the given hash
-func lookupHash(hash string, apikey string) ResultsData {
+func lookupHash(hash string, apikey string) pluginResults {
 	// NOTE: https://godoc.org/github.com/levigross/grequests
 	// fmt.Println("Getting virustotal report...")
 	ro := &grequests.RequestOptions{
@@ -191,14 +224,29 @@ func lookupHash(hash string, apikey string) ResultsData {
 	if resp.Ok != true {
 		log.Println("Request did not return OK")
 	}
-	var vtResult ResultsData
-	// fmt.Println(resp.String())
-	resp.JSON(&vtResult)
-	// fmt.Printf("%#v", vtResult)
-	t, _ := time.Parse("2006-01-02 15:04:05", vtResult.ScanDate)
-	vtResult.ScanDate = t.Format("Mon 2006Jan02 15:04:05")
 
-	return vtResult
+	var results pluginResults
+
+	resp.JSON(&results.VT.Data)
+	// resp.JSON(&vtResult)
+
+	var vtResult ResultsData
+	err = mapstructure.Decode(results.VT.Data, &vtResult)
+	assert(err)
+
+	// vtJSON, err := json.Marshal(vtResult)
+	// assert(err)
+	// // write to stdout
+	// fmt.Println(string(vtJSON))
+
+	// results.ID = "befb88b89c2eb401900a68e9f5b78764203f2b48264fcc3f7121bf04a57fd408"
+	results.ID = vtResult.Sha256
+	results.VT.Now = time.Now().Format("Mon 2006Jan02 15:04:05")
+
+	// t, _ := time.Parse("2006-01-02 15:04:05", vtResult.ScanDate)
+	// vtResult.ScanDate = t.Format("Mon 2006Jan02 15:04:05")
+
+	return results
 }
 
 func getRatio(positives int, total int) string {
@@ -227,6 +275,25 @@ func shortenPermalink(longURL string) string {
 	resp.JSON(&btl)
 
 	return btl.Data.URL
+}
+
+// writeToDatabase upserts plugin results into Database
+func writeToDatabase(results pluginResults, host string) {
+
+	address := fmt.Sprintf("%s:28015", getopt("MALICE_RETHINKDB", "rethink"))
+
+	// connect to RethinkDB
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  address,
+		Timeout:  5 * time.Second,
+		Database: "malice",
+	})
+	if err == nil {
+		// upsert into RethinkDB
+		resp, err := r.Table("samples").Insert(results, r.InsertOpts{Conflict: "replace"}).RunWrite(session)
+		assert(err)
+		log.Debug(resp)
+	}
 }
 
 var appHelpTemplate = `Usage: {{.Name}} {{if .Flags}}[OPTIONS] {{end}}COMMAND [arg...]
@@ -258,6 +325,7 @@ func main() {
 	app.Compiled, _ = time.Parse("20060102", BuildTime)
 	app.Usage = "Malice VirusTotal Plugin"
 	var apikey string
+	var rethinkdb string
 	var table bool
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
@@ -282,6 +350,13 @@ func main() {
 			EnvVar:      "MALICE_VT_API",
 			Destination: &apikey,
 		},
+		cli.StringFlag{
+			Name:        "rethinkdb",
+			Value:       "",
+			Usage:       "rethinkdb address for Malice to store results",
+			EnvVar:      "MALICE_RETHINKDB",
+			Destination: &rethinkdb,
+		},
 	}
 	app.Commands = []cli.Command{
 		{
@@ -289,7 +364,7 @@ func main() {
 			Aliases:   []string{"s"},
 			Usage:     "Upload binary to VirusTotal for scanning",
 			ArgsUsage: "FILE to upload to VirusTotal",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				// Check for valid apikey
 				if apikey == "" {
 					log.Fatal(fmt.Errorf("Please supply a valid VT_API key with the flag '--api'."))
@@ -305,6 +380,7 @@ func main() {
 				} else {
 					log.Fatal(fmt.Errorf("Please supply a file to upload to VirusTotal."))
 				}
+				return nil
 			},
 		},
 		{
@@ -312,7 +388,7 @@ func main() {
 			Aliases:   []string{"l"},
 			Usage:     "Get file hash scan report",
 			ArgsUsage: "MD5/SHA1/SHA256 hash of file",
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				// Check for valid apikey
 				if apikey == "" {
 					log.Fatal(fmt.Errorf("Please supply a valid VT_API key with the flag '--api'."))
@@ -320,17 +396,21 @@ func main() {
 
 				if c.Args().Present() {
 					vtReport := lookupHash(c.Args().First(), apikey)
-					vt := virustotal{Results: vtReport}
+					// vt := virustotal{Results: vtReport}
 					if table {
-						printMarkDownTable(vt)
+						printMarkDownTable(vtReport.VT)
 					} else {
-						vtJSON, err := json.Marshal(vt)
+						vtJSON, err := json.Marshal(vtReport)
 						assert(err)
+						// upsert into Database
+						writeToDatabase(vtReport, rethinkdb)
+						// write to stdout
 						fmt.Println(string(vtJSON))
 					}
 				} else {
 					log.Fatal(fmt.Errorf("Please supply a MD5/SHA1/SHA256 hash to query."))
 				}
+				return nil
 			},
 		},
 	}
